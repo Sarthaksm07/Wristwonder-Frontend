33d058a1bc912ac0786f45028e4a79cac5a3058a
Sarthak Maurya <126111357+Sarthaksm07@users.noreply.github.com>
2023-12-10 23:30:24 +0530

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,49 @@
+import HeroBanner from "@/components/HeroBanner";
+import ProductCard from "@/components/ProductCard";
+import Wrapper from "@/components/Wrapper";
+import { fetchDataFromApi } from "@/utils/api";
+export default function Home({ products }) {
+  return (
+    <main>
+      <HeroBanner />
+      <Wrapper>
+        {/* heading and paragaph start */}
+        <div className="text-center max-w-[800px] mx-auto my-[50px] md:my-[80px]">
+          <div className="text-[28px] md:text-[34px] mb-5 font-semibold leading-tight">
+            Time to make a statement
+          </div>
+          <div className="text-md md:text-xl">
+            Because your wrist deserves a watch that ticks all the boxes, and
+            Elevate your style with us, where every second counts in fashion.
+          </div>
+        </div>
+        {/* heading and paragaph end */}
+
+        {/* products grid start */}
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-14 px-5 md:px-0">
+          {products?.data?.map((product) => (
+            <ProductCard key={product?.id} data={product} />
+          ))}
+          {/* <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard /> */}
+        </div>
+        {/* products grid end */}
+      </Wrapper>
+    </main>
+  );
+}
+
+export async function getStaticProps() {
+  const products = await fetchDataFromApi("/api/products?populate=*");
+
+  return {
+    props: { products },
+  };
+}
